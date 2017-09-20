466eb89e7bcb5cc65bd4493cca66748770077926
Brett Jones <brettjonesdev@gmail.com>
2017-09-19 14:12:40 -1000

    Update package.json, export `formatDate` in `index.js`

--- src/index.js
+++ src/index.js
@@ -1,6 +1,3 @@
-import moment from 'moment-timezone'
+import formatDate from './formatDate'
 
-function formatDate(date, timezone, timeFormat='h:mma') {
-  return moment.utc(date).tz(timezone).format(`YYYY-MM-DD ${timeFormat}`)
-}
 export default formatDate
