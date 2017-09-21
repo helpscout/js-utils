82ec1f079ae89ae479bc87401645b219aa7aa3ee
Brett Jones <brettjonesdev@gmail.com>
2017-09-20 14:32:51 -1000

    Fix webpack library

--- webpack.config.js
+++ webpack.config.js
@@ -5,7 +5,7 @@ module.exports = {
   output: {
     path: path.resolve(__dirname, 'dist'),
     filename: 'bundle.js',
-    library: '@helpscout/blue',
+    library: '@helpscout/js-utils',
     libraryTarget: 'commonjs2'
   },
   module: {
